b8a2dd606d81f35e0bb774909bf7525a347793aa
andela-fadebayo <fiyinfoluwa.adebayo@andela.co>
2015-02-10 09:54:38 +0100

    [Fixes #86881480] Authenticated user sees My Andonation link

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,25 +1,17 @@
 require 'rails_helper'
 
 feature 'Signup/Login user' do
-  before do
+
+  scenario 'user logs in and sees My Andonation link' do
     OmniAuth.config.test_mode = true
     set_valid_omniauth
-    visit root_path
-  end
 
-  scenario 'user logs in' do
+    visit root_path
     click_on 'Login'
     expect(page).to have_content('Successfully authenticated from Google account.')
+    expect(page).to have_link('My Andonation', href: '/')
     click_on 'Logout'
     expect(page).to have_content('Signed out successfully.')
-  end
-
-  scenario 'Authenticated user sees My Andonation link. Unauthenticated user do not see My Andonation Link' do
-    click_on 'Login'
-    within 'li.andonation-link' do
-      expect(page).to have_link('My Andonation', href: '/')
-    end
-    click_on 'Logout'
-      expect(page).to have_css('li.no-andonation-link')
+    expect(page).not_to have_link('My Andonation', href: '/')
   end
 end
